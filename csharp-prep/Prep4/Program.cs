726fb5ee62e6e167e7ce54013245070a9b74b44d
isaacpacasbyui <isaacpacasbyui01@gmail.com>
2026-09-24 22:48:37 -0500

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
-using System;
+List<int> numbers = new List<int>();
 
-class Program
+Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+int number = -1;
+
+while (number != 0)
 {
-    static void Main(string[] args)
+    Console.Write("Enter number: ");
+    number = int.Parse(Console.ReadLine());
+
+    if (number != 0)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        numbers.Add(number);
     }
 }
+
+int sum = 0;
+
+foreach (int value in numbers)
+{
+    sum += value;
+}
+
+double average = (double)sum / numbers.Count;
+
+int largest = numbers[0];
+
+foreach (int value in numbers)
+{
+    if (value > largest)
+    {
+        largest = value;
+    }
+}
+
+Console.WriteLine($"The sum is: {sum}");
+Console.WriteLine($"The average is: {average}");
+Console.WriteLine($"The largest number is: {largest}");
